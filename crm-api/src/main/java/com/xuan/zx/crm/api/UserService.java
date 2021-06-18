3743a59c1c3c0330c6db526d4c747f2496f4d63b
z-xuan <zhujian199110@163.com>
2021-06-18 16:38:04 +0800

    1.0

---
+++ crm-api/src/main/java/com/xuan/zx/crm/api/UserService.java
@@ -0,0 +1,14 @@
+package com.xuan.zx.crm.api;
+
+
+import com.xuan.zx.crm.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
---
+++ crm-service/src/main/java/com/xuan/zx/crm/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.xuan.zx.crm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.beans.BeanCopier;
+import org.springframework.stereotype.Component;
+
+import com.xuan.zx.crm.dao.dataobject.UserDO;
+import com.xuan.zx.crm.dao.mapper.UserMapper;
+import com.xuan.zx.crm.api.UserService;
+import com.xuan.zx.crm.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+@Component
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    private static final BeanCopier copier = BeanCopier.create(UserModel.class, UserDO.class, false);
+
+    public String getUserName(Long id) {
+        UserDO userDO = userMapper.getById(id);
+        return userDO != null ? userDO.getName() : null;
+    }
+
+    public UserModel addUser(UserModel user) {
+        UserDO userDO = new UserDO();
+        copier.copy(user, userDO, null);
+
+        Long id = userMapper.insert(userDO);
+        user.setId(id);
+        return user;
+    }
+}
